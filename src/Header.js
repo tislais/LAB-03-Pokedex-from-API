ff2082ea91a9483d663370e7b348f7f8bcd84c57
Tis Lais <tistististis@gmail.com>
2021-04-29 12:48:20 -0700

    minor style and attempt at conditional render which did not work

--- src/Header.js
+++ src/Header.js
@@ -6,7 +6,7 @@ class Header extends Component {
   render() { 
     return (
       <header className="Header">
-        <h1>Amazing Pokemon</h1>
+        <h1>Pokedex</h1>
       </header>
     );
   }
--- src/PokemonItem.js
+++ src/PokemonItem.js
@@ -1,18 +1,39 @@
 import React, { Component } from 'react';
 import './PokemonItem.css';
 
+
+
 class PokemonItem extends Component {
+
   render() { 
     const { pokemonProp } = this.props;
     //const pokemonProp = this.props.pokemonProp; This is the same thing!
+
+    function renderTypes() {
+      if (!pokemonProp.type_2 === 'NA') {
+        return <span>{pokemonProp.type_1} · {pokemonProp.type_2}</span>;
+      } else {
+        return <span>{pokemonProp.type_1}</span>;
+      }
+    }
 
     return (
       <a href={pokemonProp.pokedex} className="PokemonItem">
         <li>
           <h2>{pokemonProp.pokemon}</h2>
           <img alt={pokemonProp.pokemon} src={pokemonProp.url_image} />
-          <p>Type 1: <span>{pokemonProp.type_1}</span></p>
-          <p>Type 2: <span>{pokemonProp.type_2}</span></p>
+          <div className="PokeTypes">
+            <span>{pokemonProp.type_1}</span>
+            <span> · {pokemonProp.type_2}</span>
+            <renderTypes />
+          </div>
+          <div className="PokeHP">Health: <span>{pokemonProp.hp}</span></div>
+          <div className="PokeStats">
+            <div>Attack: <span>{pokemonProp.attack}</span></div>
+            <div>Special: <span>{pokemonProp.special_attack}</span></div>
+            <div>Defense: <span>{pokemonProp.defense}</span></div>
+            <div>Special: <span>{pokemonProp.special_defense}</span></div>
+          </div>
         </li>
       </a>
     );
